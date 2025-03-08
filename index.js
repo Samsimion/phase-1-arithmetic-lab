c35831a1009b1772fe87000e564786e2ab2ed622
simion samuel <samuel.simion@student.moringaschool.com>
2025-03-08 06:23:33 +0300

    math assignment js

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 
-// Write your code here
+// Write your code here 
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+const random = Math.floor(Math.random()*10)+1;
+
+const num3 = 10;
+const num4 =6 ;
+const mod = num3%num4;
+
+const max = Math.max(5,4,4,5,6,8,9,4,5,7,9,12,20)
+
+
+/*describe('mod', function() {
+    it("is an equation that calculates a remainder that is equal to 4", function() {
+      expect(num3).to.be.a('number')
+      expect(num4).to.be.a('number')
+      expect(mod).to.eq(4)
+    });
+  });
+  describe('max', function() {
+    it("will return 20 as the highest number in the set", function() {
+      expect(max).to.eq(20)
+    });
+  });
+});
+ */
